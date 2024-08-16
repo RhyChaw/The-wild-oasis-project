9e5abedd134ba41d5330c7262933de3b7949e5a9
Rhythm Chawla <rhychaw@Rhythms-MacBook-Pro.local>
2024-08-17 02:48:32 +0530

    finalized changes

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+export const supabaseUrl = "https://fmbwjblmmbohnbqqrykp.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZtYndqYmxtbWJvaG5icXFyeWtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM0NjUzMjgsImV4cCI6MjAzOTA0MTMyOH0.Hv-8yarNuQee0UWufN8Dswjp2OmhWSbmx4ev4d7er4M";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
